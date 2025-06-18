4b93a6ac20d77a8a3ec1b8c2a4cb644538d0275f
RaihanSulthon <raihansy2111@gmail.com>
2025-06-18 14:27:54 +0700

    upd

--- src/pages/TicketDetailPage.jsx
+++ src/pages/TicketDetailPage.jsx
@@ -1301,7 +1301,7 @@ const TicketDetailPage = () => {
             >
               <svg className="h-4 w-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-              </svg>
+              </svg> 
               Tutup
             </button>
           </div>
