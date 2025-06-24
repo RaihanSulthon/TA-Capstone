9a4f2dde30cca7630735d3c84777ca6eaa7c3b0b
RaihanSulthon <raihansy2111@gmail.com>
2025-06-24 16:15:26 +0700

    upd

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -7,12 +7,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyDDFVi5V6bL29IxN_Ix_84HOEK2F-uNu-8",
+  authDomain: "capstone-a1019.firebaseapp.com",
+  projectId: "capstone-a1019",
+  storageBucket: "capstone-a1019.firebasestorage.app",
+  messagingSenderId: "60293545995",
+  appId: "1:60293545995:web:337c4b6a41d4b36a8e070e"
 };
 
 // Initialize Firebase
